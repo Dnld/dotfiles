ea3cd600bf757eb0578a2eaefc98a95d6155deca
Dnld <steinert.donald@gmail.com>
2020-06-04 17:58:54 -0400

    add js stuff

---
+++ js/.eslintrc.js
@@ -0,0 +1,12 @@
+var OFF = 0,
+  WARN = 1,
+  ERROR = 2;
+
+module.exports = exports = {
+  extends: ['eslint:recommended', 'google'],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+};
