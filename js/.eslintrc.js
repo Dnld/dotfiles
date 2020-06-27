5beff4e00ffb619a57f18f8f17311f0c2fbd66bb
Dnld <steinert.donald@gmail.com>
2020-06-27 05:46:30 -0400

    update preferred eslintrc

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -1,11 +1,28 @@
-module.exports = exports = {
-  extends: ['eslint:recommended', 'google'],
+module.exports = {
   env: {
     browser: true,
     es6: true,
     node: true,
   },
+  extends: ['eslint:recommended', 'google', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   rules: {
+    'arrow-parens': [1, 'as-needed'],
+    'no-invalid-this': 0,
+    indent: 0,
+    'max-len': [1, 88],
+    'no-unused-vars': 'warn',
+    'operator-linebreak': 0,
+    'object-curly-spacing': [1, 'always'],
+    'react/no-unescaped-entities': 0,
+    'react/prop-types': 1,
     'require-jsdoc': 0,
   },
 };
