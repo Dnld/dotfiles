0486e4f430c5ff170586af40c12957acc127de4c
Dnld <steinert.donald@gmail.com>
2020-08-01 16:34:58 -0400

    update default eslintrc

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -1,29 +1,31 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'google', 'plugin:react/recommended'],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
     },
-  },
-  rules: {
-    'arrow-parens': [1, 'as-needed'],
-    'no-invalid-this': 0,
-    indent: 0,
-    'max-len': [1, 88],
-    'no-empty': 1,
-    'no-unused-vars': 'warn',
-    'operator-linebreak': 0,
-    'object-curly-spacing': [1, 'always'],
-    'react/no-unescaped-entities': 0,
-    'react/prop-types': 1,
-    'require-jsdoc': 0,
-  },
+    extends: ['eslint:recommended', 'google', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+        'arrow-parens': [1, 'as-needed'],
+        'no-invalid-this': 0,
+        indent: 0,
+        "jsx-a11y/href-no-hash": "off",
+        "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
+        'max-len': [1, 88],
+        'no-empty': 1,
+        'no-unused-vars': 'warn',
+        'operator-linebreak': 0,
+        'object-curly-spacing': [1, 'always'],
+        'react/no-unescaped-entities': 0,
+        'react/prop-types': 1,
+        'require-jsdoc': 0,
+    },
 };
